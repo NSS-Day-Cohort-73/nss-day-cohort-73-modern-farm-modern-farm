f92127f9b482ec284f71518511c5c04e7ade6525
Christopher Johnson <cjohnson7741@gmail.com>
2024-09-19 15:46:46 -0500

    added algorithm comments

---
+++ src/scripts/field.js
@@ -0,0 +1,9 @@
+// define fieldArray[] as an empty array
+// define and export a function named addPlant()
+// define and export a function named usePlants() that exports a copy of the field array
+
+
+// fieldArray = []
+// fieldArray.push(plantSeeds(yearlyPlan))
+
+// export usePlants() to send field array copy to main
