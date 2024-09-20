b7a9c167fae47cee6b38c444f453bc804de7834b
Jayden Palmer <jayalexpalmer@gmail.com>
2024-09-20 14:38:09 -0500

    tractor module with working plantSeeds() sending array to addPlant()

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,9 @@
 import { createPlan } from "./plan.js";
+import { plantSeeds } from "./tractor.js";
 
 const yearlyPlan = createPlan()
 console.log("Welcome to the main thing")
+
+const plants = plantSeeds(yearlyPlan)
+
 
