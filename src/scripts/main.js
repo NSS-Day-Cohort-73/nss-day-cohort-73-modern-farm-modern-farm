9eaddb82ec3ac50daee76060b2e6b00345c7fb01
mleelaw <mleelaw123@gmail.com>
2024-09-23 10:41:19 -0500

    main logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,20 @@
+import { createPlan } from "./plan.js";
+import { plantSeeds } from "./tractor.js"
+import { usePlants } from "./field.js"
+import { harvestPlants } from "./harvester.js"
+import { catalog } from "./catalog.js"
 
-import { createAsparagus } from "./seeds/asparagus.js";
-import { createCorn } from "./seeds/corn.js";
-import { createPotato } from "./seeds/potato.js";
-
-const asparagusSeed = createAsparagus();
-console.log(asparagusSeed);
 
 const yearlyPlan = createPlan();
+
+const plantedSeeds = plantSeeds(yearlyPlan);
+
+const fieldArray = usePlants();
+
+const harvestArray = harvestPlants(fieldArray);
+
+const  htmlString = catalog(harvestArray)
+
+
  
+
