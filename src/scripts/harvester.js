183dcd00e3cd59ec3a8f13fec084a6f25575bca1
Rosemary Chadwick <rrchadwick@hotmail.com>
2024-09-23 11:24:57 -0500

    function in harvester.js added

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,3 +1,17 @@
 // export a harvestPlants() that returns an array of seed objects
 // iterate over the fieldArray and for each element add the current object to the finalFieldArray based on the output property
 //return finalFieldArray
+
+export const harvestPlants = () => {
+    const finalFieldArray = [];
+
+    for (const plant of fieldArray) {
+        let count = 0;
+
+        while (count < plant.output) {
+            finalFieldArray.push({ ...plant});
+            count++;
+        }        
+    }
+    return finalFieldArray
+}
