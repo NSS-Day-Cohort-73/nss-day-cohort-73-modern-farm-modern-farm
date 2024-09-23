7f87972c27536d6f36cd190c23f2ecca103daa24
Niles Clancy <nilesclancy@gmail.com>
2024-09-23 12:36:15 -0400

    main.js edits

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,49 @@
-console.log("Welcome to the main module")
+// console.log("Welcome to the main module")
+// imports: createPlan(), plantSeeds(), usePlants(), harvestPlants(), createCatalog()
+// variables: yearlyPlan[], fieldArray[], finalFieldArray[]
 
-// invoke createplan() from plan.js into the yearlyPlan variable
+import { createPlan } from "./plan";
+import {plantSeeds} from "./tractor.js"
+import {usePlants} from "./field.js"
+import {harvestPlants} from "./harvester.js"
+import {createCatalog} from "./catalog.js"
 
-// invoke plantSeeds(yearlyPlan)
+// invoke createPlan() from plan.js into the yearlyPlan variable
+
+// const yearlyPlan = []
+// yearlyPlan = createPlan()
+
+plantSeeds(yearlyPlan)
+
 // for each element in yearlyPlan invoke addPlant() return to variable
 
-//invoke fieldArray = usePlants()
+// yearlyPlan.forEach = (cropString) => {
+   // addPlant(cropString)
 
-//invoke finalFieldArray = harvestPlants(fieldArray)
+   //  return yearlyPlan
+// }
+// pick one, above or below 
+// for (const cropString of yearlyPlan) {
+   // addPlant(cropString)
 
-//invoke catalog(finalFieldArray) to pass the final array to the HTML builder
+   // return yearlyPlan
+// }
 
+// invoke plantSeeds(yearlyPlan)
+
+fieldArray = plantSeeds()
+
+// invoke fieldArray = usePlants()
+
+// const fieldHTML = usePlants()
+
+// invoke finalFieldArray = harvestPlants(fieldArray)
+
+finalFieldArray = []
+finalFieldArray = harvestPlants(fieldArray)
+
+// invoke catalog(finalFieldArray) to pass the final array to the HTML builder
+// return the final array after passing though catalog() to convert into an HTML string to add to the DOM using innerHTML
+
+const catalogHTML = catalog(finalFieldArray)
+catalogHTML.innerHTML = finalFieldArray
